ba7dae7349392e59917705428af7ea087bd97fa3
Chulgyun Roh <cg.roh@alticast.com>
2020-01-03 15:57:59 +0900

    add injecting js source
    - add injecting js source
      - if DEVELOPMNET is defined, injected bundle will load 'bundle.js'
    file in same path with libWPEInjectedBundle.so else hexadecimal array
    in bundle.js.h which is exported from 'bundle.js' will be injected into
    WebKit Browser
    - evaluate 'objectHooker.hook()' to hook and redefine Object Elements
    when onContentLoaded is emitted.

--- WebKitBrowser/InjectedBundle/main.cpp
+++ WebKitBrowser/InjectedBundle/main.cpp
@@ -11,6 +11,16 @@
 #include "Utils.h"
 #include "WhiteListedOriginDomainsList.h"
 
+#define DEVELOPMENT
+#ifdef DEVELOPMENT
+#include <iostream>
+#include <fstream>
+unsigned char *bundle_js = nullptr;
+unsigned int bundle_js_len = 0;
+#else
+#include "bundle.js.h"
+#endif
+
 using namespace WPEFramework;
 using JavaScript::ClassDefinition;
 using WebKit::WhiteListedOriginDomainsList;
@@ -69,6 +79,24 @@ public:
         }
 
         _whiteListedOriginDomainPairs = WhiteListedOriginDomainsList::RequestFromWPEFramework();
+
+#ifdef DEVELOPMENT
+        std::ifstream fin("/usr/share/WPEFramework/WebKitBrowser/bundle.js");
+        if (fin.good()) {
+            std::cerr << "Success to open bundle.js" << std::endl;
+            string line;
+            string bundleJS;
+            while (!fin.eof()) {
+                std::getline(fin, line);
+                bundleJS.append(line);
+                bundleJS.append("\n");
+            }
+            fin.close();
+            bundle_js_len = bundleJS.length();
+            bundle_js = (unsigned char*) malloc(bundle_js_len);
+            memcpy(bundle_js, bundleJS.c_str(), bundle_js_len);
+        }
+#endif
     }
 
     void Deinitialize()
@@ -78,6 +106,11 @@ public:
         }
 
         Core::Singleton::Dispose();
+#ifdef DEVELOPMENT
+        if (bundle_js != nullptr) {
+            free (bundle_js);
+        }
+#endif
     }
 
     void WhiteList(WKBundleRef bundle)
@@ -165,7 +198,26 @@ static WKBundlePageLoaderClientV6 s_pageLoaderClient = {
     nullptr, // didReceiveServerRedirectForProvisionalLoadForFrame
     nullptr, // didFailProvisionalLoadWithErrorForFrame
     nullptr, // didCommitLoadForFrame
-    nullptr, // didFinishDocumentLoadForFrame
+    // didFinishDocumentLoadForFrame
+    [](WKBundlePageRef, WKBundleFrameRef frame, WKTypeRef *userData, const void *) {
+        std::cerr << "didFinishDocumentLoadForFrame" << std::endl;
+
+        JSGlobalContextRef context = WKBundleFrameGetJavaScriptContext(frame);
+        JSObjectRef windowObj = JSContextGetGlobalObject(context);
+        JSStringRef objectHookerName = JSStringCreateWithUTF8CString("objectHooker");
+        JSValueRef objectHookerVal = JSObjectGetProperty(context, windowObj, objectHookerName, nullptr);
+        if (JSValueIsObject(context, objectHookerVal)) {
+            JSObjectRef objectHookerObj = JSValueToObject(context, objectHookerVal, nullptr);
+            JSStringRef hookName = JSStringCreateWithUTF8CString("hook");
+            JSValueRef hookVal = JSObjectGetProperty(context, objectHookerObj, hookName, nullptr);
+            if (JSValueIsObject(context, hookVal)) {
+                JSObjectRef hookObj = JSValueToObject(context, hookVal, nullptr);
+                JSObjectCallAsFunction(context, hookObj, nullptr, 0, nullptr, nullptr);
+            }
+            JSStringRelease(hookName);
+        }
+        JSStringRelease(objectHookerName);
+    },
     nullptr, // didFinishLoadForFrame
     nullptr, // didFailLoadWithErrorForFrame
     nullptr, // didSameDocumentNavigationForFrame
@@ -177,10 +229,28 @@ static WKBundlePageLoaderClientV6 s_pageLoaderClient = {
     nullptr, // didRunInsecureContentForFrame
     // didClearWindowObjectForFrame
     [](WKBundlePageRef, WKBundleFrameRef frame, WKBundleScriptWorldRef scriptWorld, const void*) {
+        std::cerr << "didClearWindowObjectForFrame" << std::endl;
         // Add JS classes to JS world.
         ClassDefinition::Iterator ite = ClassDefinition::GetClassDefinitions();
         while (ite.Next()) {
             InjectInJSWorld(*ite, frame, scriptWorld);
+        }
+        // Inject @alticast OIPF Objects and ObjecetHooker
+        if (bundle_js && bundle_js_len > 0) {
+            string bundleJS = string(reinterpret_cast<char *>(bundle_js), bundle_js_len);
+            JSGlobalContextRef context = WKBundleFrameGetJavaScriptContext(frame);
+            JSObjectRef windowObj = JSContextGetGlobalObject(context);
+            JSStringRef evalString = JSStringCreateWithUTF8CString("eval");
+            JSValueRef evalValue = JSObjectGetProperty(context, windowObj, evalString, nullptr);
+            JSObjectRef evalObj = JSValueToObject(context, evalValue, nullptr);
+            JSStringRef argumentString = JSStringCreateWithUTF8CString(bundleJS.c_str());
+            JSValueRef arguments[1];
+            arguments[0] = JSValueMakeString(context, argumentString);
+
+            JSObjectCallAsFunction(context, evalObj, nullptr, 1, arguments, nullptr);
+
+            JSStringRelease(evalString);
+            JSStringRelease(argumentString);
         }
     },
     nullptr, // didCancelClientRedirectForFrame
